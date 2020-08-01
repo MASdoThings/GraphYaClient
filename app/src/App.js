37a3c458745b517d4275925118d0b794d7ff149a
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2020-08-01 12:49:59 +0300

    'Force' combined with 'Drag'
    Each node has now a drag functionality.

--- app/src/App.js
+++ app/src/App.js
@@ -10,97 +10,186 @@ import {
     forceX,
     forceY,
     forceCollide,
-    forceRadial,
+    event,
     forceCenter
 } from "d3";
 import useResizeObserver from "./hooks/useResizeObserver";
 
 function App () {
     const svgRef = useRef();
+    const gRef = useRef();
     const wrapperRef = useRef();
     const dimensions = useResizeObserver(wrapperRef);
 
+    const nodes = [
+        {id: "Cravat", group: 1},
+        {id: "Count", group: 1},
+        {id: "OldMan", group: 1},
+        {id: "Lazare", group: 2},
+        {id: "Valdemar", group: 2},
+        {id: "Marguerite", group: 2},
+        {id: "Mme.deR", group: 2},
+    ];
+    const edges = [
+        {
+            source: "OldMan",
+            target: "Valdemar",
+            value: 1
+        },
+        {
+            source: "Count",
+            target: "Valdemar",
+            value: 2
+        },
+        {
+            source: "Cravat",
+            target: "Valdemar",
+            value: 1
+        },
+        {
+            source: "Lazare",
+            target: "Marguerite",
+            value: 1
+        },
+        {
+            source: "Lazare",
+            target: "Mme.deR",
+            value: 1
+        },
+        {
+            source: "Mme.deR",
+            target: "OldMan",
+            value: 1
+        },
+        {
+            source: "OldMan",
+            target: "Lazare",
+            value: 1
+        }
+    ];
 
     useEffect(() => {
         if (!dimensions) return;
-        const svg = select(svgRef.current);
-        svg.attr("viewBox", [
+
+        const svg = select(svgRef.current)
+            .attr("viewBox", [
             -dimensions.width / 2,
             -dimensions.height / 2,
             dimensions.width,
             dimensions.height
         ]);
-        const nodes = [
-            [56.94,10.06],
-            [42.04,33.23],
-            [13.98,32.70],
-            [37.87,32.62],
-            [81.15,15.44],
-            [88.47,43.54],
-            [23.08,42.67],
-            [80.71,42.63],
-            [57.56,37.10],
-            [39.64,42.51],
-        ];
 
-        const edges = [
-            {
-                source: nodes[0],
-                target: nodes[1]
-            },
-            {
-                source: nodes[3],
-                target: nodes[4]
-            },
-            {
-                source: nodes[2],
-                target: nodes[0]
-            },
-            {
-                source: nodes[5],
-                target: nodes[3]
-            }
-        ];
+        // const g = select(gRef.current)
+        //     .attr("cursor","grab");
 
-        forceSimulation(nodes)
+
+
+        const sim = forceSimulation(nodes)
             .force('charge', forceManyBody())
             .force('center', forceCenter(50, 50))
-            .force('link', forceLink())
+            .force('link', forceLink().id(function(d) { return d.id; }))
             .force('collision', forceCollide().radius(function(d) {
-                return d.radius
-            }))
-            .on('tick', () => {
-            // links
-                    svg
-                        .selectAll(".link")
-                        .data(edges)
-                        .join("line")
-                        .attr("class", "link")
-                        .attr("stroke", "black")
-                        .attr("fill", "none")
-                        .attr("x1", link => link.source.x)
-                        .attr("y1", link => link.source.y)
-                        .attr("x2", link => link.target.x)
-                        .attr("y2", link => link.target.y);
+                return d.radius || 50
+            }));
 
-                    // nodes
-                svg
-                    .selectAll(".node")
-                    .data(nodes)
-                    .join("circle")
-                    .attr("class", "node")
-                    .attr("r", 10)
-                    .attr("cx", node => node.x)
-                    .attr("cy", node => node.y);
+        const link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(edges)
+            .enter().append("line");
 
-    }
-    );
+        const node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("r", 30)
+            .attr("cursor","grab")
+            .call(drag()
+                .on("start", dragStarted)
+                .on("drag", dragged)
+                .on("end", dragEnded));
 
-    }, [ dimensions]);
+        node.append("title")
+            .text(function(d) { return d.id; });
+
+        sim
+            .force("link")
+            .links(edges)
+            .distance(300);
+
+        sim
+            .nodes(nodes)
+            .on("tick", ticked);
+
+
+
+        function ticked() {
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            node
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+        }
+
+        function dragStarted(d) {
+            if (!event.active) sim.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        function dragged(d) {
+            d.fx = event.x;
+            d.fy = event.y;
+        }
+
+        function dragEnded(d) {
+            if (!event.active) sim.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
+
+
+    //         .on('tick', () => {
+    //              // links
+    //              g
+    //                  .selectAll(".link")
+    //                  .data(edges)
+    //                  .join("line")
+    //                  .attr("class", "links")
+    //                  .attr("stroke", "black")
+    //                  .attr("fill", "none")
+    //                  .attr("x1", link => link.source.x)
+    //                  .attr("y1", link => link.source.y)
+    //                  .attr("x2", link => link.target.x)
+    //                  .attr("y2", link => link.target.y);
+    //
+    //             // nodes
+    //             g
+    //                 .selectAll(".node")
+    //                 .data(nodes)
+    //                 .join("circle")
+    //                 .attr("class", "nodes")
+    //                 .attr("r", node => node.r || 10)
+    //                 .attr("cx", node => node.x)
+    //                 .attr("cy", node => node.y);
+    //
+    // });
+
+
+
+
+    }, [edges,nodes,dimensions]);
 
     return(
         <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
-            <svg ref={svgRef}/>
+            <svg ref={svgRef}>
+
+            </svg>
         </div>
     )
 }
